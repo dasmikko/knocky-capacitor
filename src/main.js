ce26fa6cb3e8bbac95cb434191ce1a14dea3f050
Mikkel Holm Jensen <mik@danak.dk>
2022-06-22 11:07:19 +0200

    toying with ionic

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import routes from "virtual:generated-pages";
 import App from './App.vue';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
 
 const router = createRouter({
   history: createWebHistory(),
