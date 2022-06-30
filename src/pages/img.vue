2b4c4d8df3d6fda665ab5c2e771695242200a2cd
Mikkel Holm Jensen <mik@danak.dk>
2022-06-30 08:30:16 +0200

    image viewer

--- src/pages/img.vue
+++ src/pages/img.vue
@@ -23,7 +23,7 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
 import { watch, reactive, ref} from 'vue'
-import { useDrag, usePinch } from '@vueuse/gesture'
+import { useDrag, useGesture, usePinch } from '@vueuse/gesture'
 import { useMotionProperties, useSpring, useMotion } from '@vueuse/motion'
 import { useDebounceFn,  } from '@vueuse/core'
 import { interpolate } from 'popmotion'
@@ -53,8 +53,66 @@ const { set } = useSpring(motionProperties, {
   stiffness: 2000,
 })
 
+const mapper = interpolate([0, 500], [1, 2], { clamp: true })
 
-const drag = useDrag(
+const onGestureChange = (state) => {
+  if (state.last) return
+
+
+  let obj = {
+    cursor: 'grabbing'
+  }
+
+  if (state.da) {
+    if (mapper(state.da[0]) > 1) {
+      zoomed.value = true
+      obj.scale = mapper(state.da[0])
+    } else {
+      zoomed.value = false
+      obj.scale = 1
+      gesture.state.pinch.values = [0, 0]
+    }
+  }
+
+  if (zoomed.value) {
+    if (state.pinching) {
+      obj.x = (imgContainer.value.clientWidth/2) - state.origin[0]
+      obj.y = (imgContainer.value.clientHeight/2) - state.origin[1]
+      gesture.state.drag.offset = [obj.x,obj.y]
+    } else {
+      obj.x = state.offset[0]
+      obj.y = state.offset[1]
+    }
+  } else {
+    obj.x = 0
+    obj.y = 0
+    gesture.state.drag.offset = [0,0]
+  }
+
+  // update ui
+  set(obj)
+}
+
+const gesture = useGesture({
+    onDrag: (state) => onGestureChange(state),
+    onPinch: (state) => onGestureChange(state),
+  },
+  {
+    drag: {
+      rubberband: true,
+      bounds: {left: 0, right: 0, top: 0, bottom: 0},
+    }, 
+    pinch: {
+      rubberband: true,
+      bounds: {left: 0, right: 0, top: 0, bottom: 0},
+    },  
+    
+    domTarget: imgContainer,
+  },
+)
+
+
+/*const drag = useGesture(
   ({ xy: [mx,my], offset: [ox, oy], dragging }) => {
     // Only allow panning if zoomed
     if (zoomed.value) {
@@ -70,7 +128,7 @@ const drag = useDrag(
     bounds: {left: 0, right: 0, top: 0, bottom: 0},
     domTarget: imgContainer,
   },
-)
+)*/
 
 const onImageLoad = (e) => {
   const height = imageElRef.value.clientHeight
@@ -80,7 +138,14 @@ const onImageLoad = (e) => {
   let topBound = (height/2)
   let leftBound = width/2
 
-  drag.config.drag.bounds = [
+  console.log(gesture)
+
+  gesture.config.drag.bounds = [
+    [-leftBound, leftBound],
+    [-topBound, topBound]
+  ]
+
+  gesture.config.pinch.bounds = [
     [-leftBound, leftBound],
     [-topBound, topBound]
   ]
@@ -88,7 +153,7 @@ const onImageLoad = (e) => {
 
 const debouncedFn = useDebounceFn(() => {
   clickCount.value = 0
-}, 1000)
+}, 800)
 
 const onClick = (e) => {
   clickCount.value++
@@ -97,7 +162,7 @@ const onClick = (e) => {
     zoomed.value = !zoomed.value
 
     if (!zoomed.value) {
-      drag.state.drag.offset = [0,0]
+      gesture.state.drag.offset = [0,0]
       set({
         cursor: 'grabbing',
         x: 0,
@@ -107,7 +172,7 @@ const onClick = (e) => {
     } else {
       let newOffsetX = (imgContainer.value.clientWidth/2) - e.layerX
       let newOffsetY = (imgContainer.value.clientHeight/2) - e.layerY
-      drag.state.drag.offset = [newOffsetX,newOffsetY]
+      gesture.state.drag.offset = [newOffsetX,newOffsetY]
 
       set({
         cursor: 'grabbing',
@@ -123,7 +188,7 @@ const onClick = (e) => {
 
 
 // Pinch
-const springValues = reactive({
+/*const springValues = reactive({
   zoom: 0,
   x: 0,
   y: 0
@@ -170,6 +235,7 @@ watch(
     deep: true,
   },
 )
+*/
 
 </script>
 
