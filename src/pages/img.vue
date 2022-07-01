7289434e456ac7427781da386f467ab624320346
Mikkel Holm Jensen <mik@danak.dk>
2022-07-01 08:44:05 +0200

    img stuff

--- src/pages/img.vue
+++ src/pages/img.vue
@@ -10,21 +10,18 @@
     </ion-header>
 
     <ion-content>
-      <div class="h-full overflow-hidden">
-        <div class="h-full flex justify-center items-center imgContainer" ref="imgContainer">
-          <img class="target" ref="imageElRef" :src="route.query.url" alt="">
-
+      <div class="h-full flex justify-center items-center overflow-hidden" ref="imgContainerRef">
+          <img
+            class="relative"
+            ref="imgRef"
+            :src="route.query.url" alt=""
+            :style="{
+              left: `${imgStyle.x}px`,
+              top: `${imgStyle.y}px`,
+              transform: `scale(${imgStyle.scale})`,
+            }"
+          >
         </div>
-      </div>
-      <Moveable
-        className="moveable"
-        v-bind:target="['.target']"
-        v-bind:draggable="true"
-        v-bind:pinchable="true"
-        @drag="onDrag"
-        @scale="onScale"
-        @pinch="onPinch"
-      />
     </ion-content>
   </ion-page>
 </template>
@@ -33,7 +30,7 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
 import { watch, reactive, ref} from 'vue'
-import Moveable from "vue3-moveable";
+import { useGesture } from '@vueuse/gesture'
 
 
 // This starter template is using Vue 3 <script setup> SFCs
@@ -41,19 +38,81 @@ import Moveable from "vue3-moveable";
 
 const router = useRouter();
 const route = useRoute();
+const imgRef = ref(null)
+const imgContainerRef = ref(null)
 
-const onDrag = ({ transform, target }) => {
-  target.style.transform = transform;
+const imgStyle = reactive({
+  x: 0,
+  y: 0,
+  scale: 2
+})
+
+const maybeAdjustImage = () => {
+  let newCrop = imgStyle;
+  let imageBounds = imgRef.value.getBoundingClientRect();
+  let containerBounds = imgContainerRef.value.getBoundingClientRect();
+  console.log(containerBounds)
+  let originalWidth = imgRef.value.clientWidth;
+  let widthOverhang = (imageBounds.width - originalWidth) / 2;
+  let originalHeight = imgRef.value.height;
+  let heightOverhang = (imageBounds.height - originalHeight) / 2;
+
+  if (imageBounds.left > containerBounds.left) {
+    newCrop.x = widthOverhang / imgStyle.scale;
+  } else if (imageBounds.right < containerBounds.right) {
+    newCrop.x = -(imageBounds.width - containerBounds.width) + widthOverhang;
+  }
+
+  imgStyle.x = newCrop.x
+  imgStyle.y = newCrop.y
 }
 
-const onScale = ({ drag, target }) => {
-  target.style.transform = drag.transform;
-}
+useGesture(
+  {
+    onDrag: (state) => {
+      let imageBounds = imgRef.value.getBoundingClientRect();
+      let containerBounds = imgContainerRef.value.getBoundingClientRect();
 
-const onPinch = ({ transform, target }) => {
-  console.log('pinch')
-  target.style.transform = transform;
-}
+      console.log(Math.ceil(imageBounds.top), containerBounds.top)
+      console.log(state.movement[1], state.direction[0])
+
+      if (Math.ceil(imageBounds.top) > containerBounds.top && state.direction[1] < 0) {
+        imgStyle.y = state.movement[1]
+      }
+
+      if (Math.ceil(imageBounds.bottom) < containerBounds.bottom && state.direction[1] > 0) {
+        imgStyle.y = state.movement[1]
+      }
+
+      if (Math.ceil(imageBounds.left) < containerBounds.left && state.direction[0] > 0) {
+        imgStyle.x = state.movement[0]
+      }
+      if (Math.ceil(imageBounds.right) > containerBounds.right && state.direction[0] < 0) {
+        imgStyle.x = state.movement[0]
+      }
+
+
+
+    },
+    onPinch: (state) => {
+      imgStyle.scale = 1 + state.offset[0] / 70
+    },
+  },
+  {
+    drag: {
+      initial: () => [imgStyle.x, imgStyle.y]
+    },
+    pinch: {
+      distanceBounds: { min: 0 },
+    },
+    domTarget: imgRef,
+    eventOptions: {
+      passive: false
+    }
+  }
+)
+
+
 </script>
 
 
