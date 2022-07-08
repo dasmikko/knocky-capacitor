acb50381026262e424d4ca8495fa3f45f96af814
dasmikko <dasmikko@gmail.com>
2022-07-08 23:12:50 +0200

    login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,17 +1,22 @@
 <template>
   <ion-page>
-  <ion-header>
-    <ion-toolbar >
-      <ion-buttons slot="start">
-        <ion-back-button default-href="/"></ion-back-button>
-      </ion-buttons>
-      <ion-title>Login</ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content>
-    Login screen
-    <IonButton @click="onClickLogin">Login</IonButton>
-  </ion-content>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <p>This isn't working yet, it'a just for testing!</p>
+      <div class="flex flex-col px-4 justify-center h-full">
+        <IonButton @click="onClickLogin('google')">Google</IonButton>
+        <IonButton @click="onClickLogin('twitter')">Twitter</IonButton>
+        <IonButton @click="onClickLogin('github')">Github</IonButton>
+        <IonButton @click="onClickLogin('steam')">Steam</IonButton>
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -46,13 +51,13 @@ export default {
     IonButton
   },
   setup () {
-    const onClickLogin = async () => {
+    const onClickLogin = async (provider) => {
       const browser = Browser
       console.log(browser)
       browser.addListener('browserPageLoaded', (data) => {
         console.log('tesst', data)
       })
-      browser.open({ url: 'https://api.knockout.chat/auth/google/login' })
+      browser.open({ url: `https://api.knockout.chat/auth/${provider}/login` })
       console.log('finish')
     }
 
