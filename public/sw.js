4028c91bda912c3fa00117c57d918964c2611a08
dasmikko <dasmikko@gmail.com>
2022-07-09 22:04:12 +0200

    remove service worker file

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('fetch', async (event) => {
-    const url = event.request.url;
-    console.log(url)
-    console.log(self)
-    console.log(await cookieStore.getAll())
-    console.log(await event)
-})
